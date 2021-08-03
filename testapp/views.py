037a1a32da07ff4b4ccbf781ce5e01642ea45ab1
gyudon <duck9912@naver.com>
2021-08-03 13:30:39 +0900

    동규인사

--- testapp/views.py
+++ testapp/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import HttpResponse
 # Create your views here.
 def welcome(request):
     return HttpResponse("<h1>테스트 앱입니다.</h1>")
+
+def ddong_Hi(request) :
+    return HttpResponse("<h2>안녕하세요! 팀원 정동규입니다.<h2>")
