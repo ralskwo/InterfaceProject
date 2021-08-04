73408973a6de922eb4aea32895c153efcd1c827c
Ruoire <sungpsych@naver.com>
2021-08-04 17:00:01 +0900

    'sidebar'

--- workspace/static/js/scripts.js
+++ workspace/static/js/scripts.js
@@ -52,3 +52,12 @@ window.addEventListener('DOMContentLoaded', event => {
     });
 
 });
+    function openNav() {
+        document.getElementById("mySidebar").style.width = "250px";
+        document.getElementById("main").style.marginLeft = "250px";
+    };
+
+    function closeNav() {
+        document.getElementById("mySidebar").style.width = "0";
+        document.getElementById("main").style.marginLeft = "0";
+    };
