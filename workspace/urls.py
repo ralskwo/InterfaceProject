5c6ff876dd82d9d3ec48af2db7fe725770803bd6
raining <iwoojoo329@gmail.com>
2021-08-04 17:43:43 +0900

    'add_princesstemplate'

--- workspace/urls.py
+++ workspace/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import  views
 
 urlpatterns = [
-    path('index/', views.index, name='index'),
+    path('home/', views.home, name='home'),
+    path('princess/', views.princess, name='princess'),
 ]
--- workspace/views.py
+++ workspace/views.py
@@ -2,7 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
-def index(request) :
-    return render(request, 'index.html')
+def home(request) :
+    return render(request, 'home.html')
+
+def princess(request) :
+    return render(request, 'princess.html')
+
+
 
 # Create your views here.
