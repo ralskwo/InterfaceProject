5e7d25e7553d368e904438c046baa774b700cb31
ralskwo <ralskwo@naver.com>
2021-08-05 10:04:04 +0900

    add image

--- workspace/views.py
+++ workspace/views.py
@@ -2,8 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
 
+
 def home(request) :
     return render(request, 'home.html')
+
 
 def princess(request) :
     return render(request, 'princess.html')
