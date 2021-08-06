e861efd7e5194cd57db17cf844b6a400ae7e2cea
Ruoire <sungpsych@naver.com>
2021-08-06 14:06:37 +0900

    'primarykey_setting'

--- workspace/models.py
+++ workspace/models.py
@@ -3,14 +3,16 @@ from django.db import models
 # Create your models here.
 
 class DBCountry(models.Model):
+    id = models.AutoField(primary_key=True)
     country = models.CharField(max_length=20, unique=True)
     latitude = models.DecimalField(max_digits=8, decimal_places=5)
     longitude = models.DecimalField(max_digits=8, decimal_places=5)
 
 class DBPrincess(models.Model):
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=15)
     age = models.IntegerField(null=True)
-    country = models.ForeignKey(DBCountry, to_field='country', on_delete=models.CASCADE)
+    country = models.ForeignKey(DBCountry, on_delete=models.CASCADE)
 
 class DBQuoats(models.Model):
     princess = models.ForeignKey(DBPrincess, on_delete=models.CASCADE)
