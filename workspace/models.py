76e5e101d35a68f34e5c213dffd0d917bc759728
Ruoire <sungpsych@naver.com>
2021-08-06 09:44:42 +0900

    'DBCouintryUniquekEY'

--- workspace/models.py
+++ workspace/models.py
@@ -2,22 +2,22 @@ from django.db import models
 
 # Create your models here.
 
+class DBCountry(models.Model):
+    country = models.CharField(max_length=20, unique=True)
+    latitude = models.DecimalField(max_digits=8, decimal_places=5)
+    longitude = models.DecimalField(max_digits=8, decimal_places=5)
+
 class DBPrincess(models.Model):
     name = models.CharField(max_length=15)
     age = models.IntegerField(null=True)
-    country = models.CharField(max_length=20, unique=True)
-
-class DBCountry(models.Model):
-    country = models.ForeignKey(DBPrincess, to_field='country', on_delete=models.CASCADE)
-    latitude = models.DecimalField(max_digits=8, decimal_places=5)
-    longitude = models.DecimalField(max_digits=8, decimal_places=5)
+    country = models.ForeignKey(DBCountry, to_field='country', on_delete=models.CASCADE)
 
 class DBQuoats(models.Model):
     princess = models.ForeignKey(DBPrincess, on_delete=models.CASCADE)
     quoats = models.TextField()
 
 class DBScene(models.Model):
-    scene= models.ForeignKey(DBPrincess, on_delete=models.CASCADE)
+    scene = models.ForeignKey(DBPrincess, on_delete=models.CASCADE)
     scenes = models.TextField()
 
 class DBSong(models.Model):
