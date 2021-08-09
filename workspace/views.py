8d86ca12d85d99a12bd1dc25386ec609241be6aa
ralskwo <ralskwo@naver.com>
2021-08-09 17:12:02 +0900

    map

--- workspace/views.py
+++ workspace/views.py
@@ -1,25 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.template import loader
-from .models import DBCountry, DBPrincess
+from .models import DBPrincess
 from os import listdir
 
 def home(request) :
-    lat = [51.11318, 57.07817, -17.66184, 39.67106, 51.11318, 46.50811, 46.50811, 17.74521, 61.90783, 61.90783, 46.50811, 23.81171, 29.95436, 37.22095];
-    lng = [10.35945, -4.33844, -149.42595, 106.88659, 10.35945, 1.88469, 1.88469, -64.77499, 9.28676, 9.28676, 1.88469, 54.09576, -90.07869, -76.74513];
-    hname = ['Rapunzel', 'Merida', 'Moana', 'Mulan', 'SnowWhite', 'Belle', 'Cinderella', 'Ariel', 'Anna', 'Elsa', 'Aurora', 'Jasmine', 'Tiana', 'Pocahontas'];
-
     # 메인 페이지
-    full_name = [i for i in listdir("workspace/static/assets/img/Main1/")]
-    princess_name = [name[:name.index('.')] for name in full_name]
+    full_name_main1 = [i for i in listdir("workspace/static/assets/img/Main1/")]
+    princess_name1 = [name[:name.index('.')] for name in full_name_main1]
+    full_name_main2 = [i for i in listdir("workspace/static/assets/img/Main1/")]
+    princess_name2 = [name[:name.index('.')] for name in full_name_main2]
 
     # 지도
-
-    full_names = [j for j in listdir("workspace/static/assets/img/Main2/")]
-    print(full_names)
+    princess_list = DBPrincess.objects.all()
+    princess_list = [["static/assets/img/Markers/" + p.name + ".png", float(p.country.latitude), float(p.country.longitude)] for p in princess_list]
 
     context = {
-        'lat': lat, 'lng': lng, 'hname': hname, 'princess_name': princess_name,
+        'princess_name1': princess_name1, 'princess_name2': princess_name2, 'princess_list': princess_list,
     }
 
     return render(request, "home.html", context)
@@ -41,7 +38,7 @@ def test(request):
         princess_dict[p.name] = {'lat': float(p.country.latitude), 'lng': float(p.country.longitude)}
         print(p.name, princess_dict[p.name])
 
-    context = {"princess_dict": princess_dict}
+    context = {"princess_dict": princess_dict, "princess_name": princess_name}
 
     return render(request, "test.html", context)
 
