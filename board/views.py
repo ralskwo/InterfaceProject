897c44dd45ca38658e8d491ff76cee8c7bd1d99c
Ruoire <sungpsych@naver.com>
2021-08-14 17:09:54 +0900

    'edited_all'

--- board/views.py
+++ board/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.utils import timezone
 from django.core.paginator import Paginator
+from os import listdir
 
 # board_list
 def index(request) :
+    full_name_main1 = [i for i in listdir("workspace/static/assets/img/Main1/")]
+    princess_name1 = [name[:name.index('.')] for name in full_name_main1]
+
     #목록
     page = request.GET.get('page', 'detail/1')
     question_list = Question.objects.order_by('-create_date')
@@ -14,16 +18,19 @@ def index(request) :
 
     isUser = request.user.is_authenticated
 
-    context = {'question_list': page_obj, 'isUser': isUser}
+    context = {'question_list': page_obj, 'isUser': isUser, 'princess_name1': princess_name1}
     return render(request, 'board_list.html', context)
 
 
 
 # board_detail
 def detail(request, question_id) :
+    full_name_main1 = [i for i in listdir("workspace/static/assets/img/Main1/")]
+    princess_name1 = [name[:name.index('.')] for name in full_name_main1]
+
     #내용 출력
     question = Question.objects.get(id=question_id)
-    context = {'question': question}
+    context = {'question': question, 'princess_name1': princess_name1}
     return render(request, 'board_detail.html', context)
 
 
@@ -42,6 +49,9 @@ def answer_create(request, question_id) :
 
 # myboard_create
 def question_create(request):
+    full_name_main1 = [i for i in listdir("workspace/static/assets/img/Main1/")]
+    princess_name1 = [name[:name.index('.')] for name in full_name_main1]
+
     #질문등록
     if request.method == 'POST' :
         title = request.POST.get('title')
@@ -50,7 +60,7 @@ def question_create(request):
         user= request.user
         Result = Question(subject=title, content=content, create_date=create_date, user=user)
         Result.save()
-        context = { "msg" : "저장 완료"  }
+        context = { "msg" : "저장 완료", "princess_name1": princess_name1}
     else :
-        context = None
+        context = {"msg": None, "princess_name1": princess_name1}
     return render(request, 'myboard_create.html', context)
